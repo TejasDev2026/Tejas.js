43139e5122ea9332f4e1560638d9416a2e5021c9
TejasDev2026 <tejasghondage@gmail.com>
2024-06-19 05:56:18 +0000

    object conversion

---
+++ 03.objectcConversion.js
@@ -0,0 +1,9 @@
+// let score="659";
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
+let isLoggedIn=15662;
+console.log(typeof isLoggedIn);
+let valueInString=String(isLoggedIn);
+console.log(valueInString);
+console.log(typeof valueInString);
