10cea659f339e69e8d114725ebfd02b370c196de
TejasDev2026 <tejasghondage@gmail.com>
2024-06-19 04:30:26 +0000

    datatypes and variables

---
+++ 01.Basics/02.Datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+// alert(3+30) here we are using node js not browser !
+console.log(9+6) 
+// first prority to code readability..!
+console.log("Tejas!")   
+// Datatypes
+/*
+number:---> 2 to the power 53
+bigint:--->
+string:--->
+undefined:--->
+null:--->
+boolean:--->
+symbol:--->
+object:--->
+*/
+console.log(typeof  Boolean)
---
+++ 01.Basics/variables.js
@@ -0,0 +1,11 @@
+const accountID=122312;
+let accountGamil="tejas@gamil.com"
+var accountPassword="312133"
+accountCity="pune"
+// accountID = 2345;
+// console.log(accountID);/
+accountGamil="qefqefdcdcadc"
+accountPassword="23123"
+accountCity="nagar"
+let accountState;
+console.table([accountID,accountPassword,accountGamil,accountCity,accountState])
